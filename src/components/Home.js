599883461a615d244b2f42ad9c5fb7f90c4f9f8b
Ivan Barrios <ivanbar8112003@gmail.com>
2023-02-14 03:45:22 -0300

    Home Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
+import "../styles/home.css";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div className="company-details">
+        <h1>Wodor</h1>
+        <h2>Your new door to the wood world!</h2>
+      </div>
+      <Link to="/shop" className="shop-btn">
+        Shop Now!
+      </Link>
     </div>
   );
 };
