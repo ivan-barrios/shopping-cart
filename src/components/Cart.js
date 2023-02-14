e8a536527d49f9098c12d110242250d5272acfad
Ivan Barrios <ivanbar8112003@gmail.com>
2023-02-14 19:15:24 -0300

    Cart working

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,27 +3,17 @@ import CartProduct from "./CartProduct";
 const Cart = (props) => {
   const { cart } = props;
 
-  const emptyCart = cart.length < 1;
-
   return (
     <div className="cart">
       <h1>Cart</h1>
       <div className="cart-container">
-        <IsEmpty emptyCart={emptyCart} cart={cart} />
+        {cart.map((product) => {
+          return <CartProduct key={product.id} product={product} />;
+        })}
       </div>
       <button>Buy Now</button>
     </div>
   );
-};
-
-const IsEmpty = (props) => {
-  if (props.emptyCart) {
-    return <h2>There are no products in the cart</h2>;
-  } else {
-    props.cart.map((product) => {
-      return <CartProduct key={product.id} product={product} />;
-    });
-  }
 };
 
 export default Cart;
